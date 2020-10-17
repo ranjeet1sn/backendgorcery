152d1977907afb3b8cfaf2589d7422ff686ee9a4
ranjeet1sn <sranjeet022@gmail.com>
2020-10-17 11:38:37 +0530

    Commit

---
+++ monogpratice/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const port = process.env.PORT || 3002;
+app.use(bodyparser.json());
+app.use(
+  bodyparser.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+);
+
+const cors = require("cors");
+const rtsIndex = require("./routes/router");
+app.use(cors({ origin: "http://localhost:4200" }));
+
+app.use(rtsIndex);
+require("./config/connection");
+
+app.listen(`${port}`, (req, res) => {
+  console.log("listen", port);
+});
